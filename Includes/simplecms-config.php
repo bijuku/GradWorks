ff560df7b12e6949d02cd418c5e240efbd9f9f30
bijuku <bijuku@hotmail.com>
2013-06-09 12:57:36 -0400

    Initial commit

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms138');
+    define('DB_USER', 'simplecmsuser138');
+    define('DB_PASSWORD', '-5o[zxkH3*e^');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'EasyProfessor');
+?>
