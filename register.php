ec5e0a52083b3056ce3dfa7a84241cf90f1f7e31
Elijah Murray <elijahmurray@gmail.com>
2013-06-09 14:59:51 -0400

    sign up/login pages are responsive

--- register.php
+++ register.php
@@ -8,33 +8,20 @@
             <section class="columns large-10 large-centered">
                 <h1>Sign Up</h1>
                 <form action="register.php" id="register" method="post">
-                    <table border="0">
-                        <tbody>
-
-                        <tr>
-                            <!-- <td><label for="name">Name</label> </td> -->
-                            <td><input id="name" maxlength="45" name="name" value="Name" type="text" /> </td>
-                        </tr>
-
-                        <!-- <tr>
-                            <td><label for="email">Prefered Contact Email</label> </td>
-                            <td><input id="email" maxlength="45" name="email" type="text" /></td>
-                        </tr> -->
-
-                        <tr>
-                            <!-- <td><label for="password">Password*:</label></td> -->
-                            <td><input id="password" maxlength="45" name="password" value="password" type="password" /></td>
-                        </tr>
-
-                        <tr>
-                            <td align="right">
+                    <section class="columns large-12">
+                        <input id="email" maxlength="45" name="email" type="text" value=".edu email" />
+                        <input id="name" maxlength="45" name="name" value="Name" type="text" />
+                        <input id="password" maxlength="45" name="password" value="password" type="password" />
+                        <div class="divider"></div>
+                        <section class="columns large-12">
+                            <div class="columns large-6">
                                 <a href="signin.php"><div class="signup alt-btn">Just logging in?</div></a>
+                            </div>
+                            <div class="columns large-6">
                                 <input name="Submit" type="submit" value="Submit" />
-                            </td>
-
-                        </tr>
-
-                    </tbody></table>
+                            </div>
+                        </section>
+                    </section>
                 </form>
             </section>
         
--- signin.php
+++ signin.php
@@ -8,27 +8,19 @@
             <section class="columns large-10 large-centered">
                 <h1>Sign In</h1>
                 <form action="signin.php" id="signin" method="post">
-                    <table border="0">
-                        <tbody>
-
-                        <tr>
-                            <td><input id="email" maxlength="45" name="email" type="text" value=".edu email" /></td>
-                        </tr>
-
-                        <tr>
-                            <!-- <td><label for="password">Password*:</label></td> -->
-                            <td><input id="password" maxlength="45" name="password" value="password" type="password" /></td>
-                        </tr>
-
-                        <tr>
-                            <td align="right">
+                   <section class="columns large-12">
+                        <input id="email" maxlength="45" name="email" type="text" value=".edu email" />
+                        <input id="password" maxlength="45" name="password" value="password" type="password" />
+                        <div class="divider"></div>
+                        <section class="columns large-12">
+                            <div class="columns large-6">
                                 <a href="register.php"><div class="signup alt-btn">Need to sign up?</div></a>
+                            </div>
+                            <div class="columns large-6">
                                 <input name="Submit" type="submit" value="Submit" />
-                            </td>
-
-                        </tr>
-
-                    </tbody></table>
+                            </div>
+                        </section>
+                    </section>
                 </form>
             </section>
         
